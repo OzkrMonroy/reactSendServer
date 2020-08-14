63bd68591804606a63c3d6e80abfae1bca621c4c
Oscar Monroy <oscarmonroy657@gmail.com>
2020-08-14 11:31:05 -0600

    Create User is ready

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,23 @@
+const User = require("../models/User")
+const bcrypt = require('bcrypt')
+
 exports.createUser = async (req, res) => {
-  console.log('From create user');
+  const { userEmail, userPassword } = req.body
+
+  let user = await User.findOne({ userEmail })
+
+  if(user) {
+    return res.status(400).json({msg: "This user already exists"});
+  }
+
+  user = new User(req.body);
+  const salt = await bcrypt.genSalt(10);
+  user.userPassword = await bcrypt.hash(userPassword, salt);
+  
+  try {
+    await user.save();
+    res.json({msg: 'Create user success'});
+  } catch (error) {
+    console.log(error);
+  }
 }
